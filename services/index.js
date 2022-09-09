0f94a82a3e570545282782ea735e7b415c47a698
Murad khan <murad.jaddoon@gmail.com>
2022-09-09 21:47:44 +0500

    created services

---
+++ services/index.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+function getPlayers() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("students")
+      .then((res) => resolve(res))
+      .catch((err) => {
+        if (err && err.response) {
+          reject(err.message);
+        }
+      });
+  });
+}
+
+
+function addPlayer(body) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`students`, body)
+      .then((res) => resolve(res))
+      .catch((err) => {
+        if (err && err.response) {
+          reject(err.message);
+        }
+      });
+  });
+}
+function updatePlayer(playerId,body) {
+  return new Promise((resolve, reject) => {
+    axios
+      .put(`students/${playerId}`, body)
+      .then((res) => resolve(res))
+      .catch((err) => {
+        if (err && err.response) {
+          reject(err.message);
+        }
+      });
+  });
+}
+
+function deletePlayer(playerId) {
+  return new Promise((resolve, reject) => {
+    axios
+      .delete(`students/${playerId}`)
+      .then((res) => resolve(res))
+      .catch((err) => {
+        if (err && err.response) {
+          reject(err.message);
+        }
+      });
+  });
+}
+export { getPlayers,addPlayer, updatePlayer, deletePlayer };
