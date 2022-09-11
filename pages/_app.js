bbf8eb305e553ea6736a498a39584ec670243469
Murad khan <murad.jaddoon@gmail.com>
2022-09-11 23:39:31 +0500

    setup axios

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import Axios from "axios";
+
+Axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_BASE_URl}`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
