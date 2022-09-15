04b3728bb8456c42f1a784c737d17ac4c8a9ceea
Murad khan <murad.jaddoon@gmail.com>
2022-09-15 18:54:57 +0500

    created layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import Axios from "axios";
+import Layout from "../layout";
 
 Axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_BASE_URl}`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
