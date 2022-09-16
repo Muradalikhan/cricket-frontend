5127dc2434020cf2d8c21ccfa5b2f22b309a81d4
Murad khan <murad.jaddoon@gmail.com>
2022-09-16 13:32:22 +0500

    added classes at layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,23 @@ import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import Axios from "axios";
 import Layout from "../layout";
+import { useState, createContext, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+
 
 Axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_BASE_URl}`;
-
+export const AppContext = createContext();
 function MyApp({ Component, pageProps }) {
+  const [playersList, setPlayersList] = useState([]);
+
+  useEffect(() => {}, [playersList]);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppContext.Provider value={{ playersList, setPlayersList }}>
+      <Layout>
+        <Component {...pageProps} />
+        <ToastContainer autoClose={1000} position="bottom-right" />
+      </Layout>
+    </AppContext.Provider>
   );
 }
 
